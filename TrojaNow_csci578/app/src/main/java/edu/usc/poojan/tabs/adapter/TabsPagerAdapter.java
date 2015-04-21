d19882f99b856f4877663f2b4952d38e56d65d02
Poojan Jhaveri <poojanjhaveri@hotmail.com>
2015-04-20 21:10:10 -0700

    First Commit. Got Activity Bar and Swipe able tabbar working

---
+++ TrojaNow_csci578/app/src/main/java/edu/usc/poojan/tabs/adapter/TabsPagerAdapter.java
@@ -0,0 +1,39 @@
+package edu.usc.poojan.tabs.adapter;
+
+/**
+ * Created by poojan on 4/20/15.
+ */
+import edu.usc.poojan.trojanow.FeedFragment;
+import edu.usc.poojan.trojanow.ProfileFragment;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class TabsPagerAdapter extends FragmentPagerAdapter {
+
+    public TabsPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int index) {
+
+        switch (index) {
+            case 0:
+                // Top Rated fragment activity
+                return new FeedFragment();
+            case 1:
+                return new ProfileFragment();
+        }
+
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        // get item count - equal to number of tabs
+        return 2;
+    }
+
+}
---
+++ TrojaNow_csci578/app/src/main/java/edu/usc/poojan/trojanow/ActionBarTabController.java
@@ -0,0 +1,68 @@
+package edu.usc.poojan.trojanow;
+
+import android.app.ActionBar;
+import android.app.FragmentTransaction;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import edu.usc.poojan.tabs.adapter.TabsPagerAdapter;
+
+
+public class ActionBarTabController extends FragmentActivity implements ActionBar.TabListener {
+
+    private ViewPager viewPager;
+    private TabsPagerAdapter mAdapter;
+    private ActionBar actionBar;
+    // Tab titles
+    private String[] tabs = { "Feed", "Profile"};
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        System.out.println("In Activity");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed);
+
+        // Initilization
+        viewPager = (ViewPager) findViewById(R.id.pager);
+        actionBar = this.getActionBar();
+        mAdapter = new TabsPagerAdapter(getSupportFragmentManager());
+
+        viewPager.setAdapter(mAdapter);
+        actionBar.setHomeButtonEnabled(false);
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        // Adding Tabs
+        for (String tab_name : tabs) {
+            actionBar.addTab(actionBar.newTab().setText(tab_name)
+                    .setTabListener(this));
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_feed, menu);
+        return true;
+    }
+
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+    }
+}

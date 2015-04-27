d98b3cd8f25a57a80f973e62b3bc0406a6f5b7f9
Trina Gregory <trinagre@usc.edu>
2015-04-27 16:34:14 -0700

    Working on SensorActivity

--- TrojaNow_csci578/app/src/main/java/edu/usc/poojan/trojanow/SensorActivity.java
+++ TrojaNow_csci578/app/src/main/java/edu/usc/poojan/trojanow/SensorActivity.java
@@ -1,5 +1,6 @@
 package edu.usc.poojan.trojanow;
 
+import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.hardware.Sensor;
@@ -17,7 +18,7 @@ public class SensorActivity extends Activity implements SensorEventListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        //setContentView(R.layout.main);
 
         // Get an instance of the sensor service, and use that to get an instance of
         // a particular sensor.
@@ -46,5 +47,29 @@ public class SensorActivity extends Activity implements SensorEventListener {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        float millibars_of_pressure = event.values[0];
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    protected void onResume() {
+        // Register a listener for the sensor.
+        super.onResume();
+        mSensorManager.registerListener(this, mPressure, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        // Be sure to unregister the sensor when the activity pauses.
+        super.onPause();
+        mSensorManager.unregisterListener(this);
     }
 }

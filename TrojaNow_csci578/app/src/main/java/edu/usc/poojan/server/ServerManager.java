f5acd90a793acdb14ec7ed97bc90b04418b26c22
Trina Gregory <trinagre@usc.edu>
2015-04-28 14:24:19 -0700

    Adding User & ServerManager

---
+++ TrojaNow_csci578/app/src/main/java/edu/usc/poojan/server/ServerManager.java
@@ -0,0 +1,23 @@
+package edu.usc.poojan.server;
+
+import com.parse.Parse;
+
+/**
+ * Created by Trina Gregory on 4/28/15.
+ */
+public class ServerManager {
+    static String PARSE_APPLICATION_ID = "fI9EePHkFfD9GHUmNkoQtF9fgSqTFin3wyeupWuF";
+    static String PARSE_CLIENT_KEY = "aRm6wWylGELtNY6vmzk5sviXRw31axHoZJFTH2JL";
+
+    public ServerManager (Object context) {
+        Parse.enableLocalDatastore( context);
+        Parse.initialize(this, PARSE_APPLICATION_ID, PARSE_CLIENT_KEY);
+
+        // Testing Parse - it works
+        /*
+        ParseObject testObject = new ParseObject("TestObject");
+        testObject.put("foo", "bar");
+        testObject.saveInBackground();
+        */
+    }
+}

1856c072796da2c3783302f71400a95962078b2d
Poojan Jhaveri <poojanjhaveri@hotmail.com>
2015-04-28 14:08:17 -0700

    Now able to register user through Parse inside register

--- TrojaNow_csci578/app/src/main/java/edu/usc/poojan/trojanow/RegisterActivity.java
+++ TrojaNow_csci578/app/src/main/java/edu/usc/poojan/trojanow/RegisterActivity.java
@@ -1,6 +1,8 @@
 package edu.usc.poojan.trojanow;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -9,6 +11,10 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 
 public class RegisterActivity extends Activity {
@@ -33,19 +39,66 @@ public class RegisterActivity extends Activity {
         RegisterButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               // Register User Here
-                System.out.println("name "+nameTextField.getText().toString()+"email address"+emailTextField.getText().toString()+"password"+passwordTextField.getText().toString());
+               // Register Button Clicked
+               // Check if any of the textfield is empty
+              if(nameTextField.getText().length()>0 && emailTextField.getText().length()>0 && passwordTextField.getText().length()>0)
+              {
 
-            }
-        });
-    }
+                  // Registering User
+                  ParseUser user = new ParseUser();
+                  user.setUsername(emailTextField.getText().toString());
+                  user.setPassword(passwordTextField.getText().toString());
+                  user.setEmail(emailTextField.getText().toString());
+                  user.put("Name", nameTextField.getText().toString());
+
+                  // If we don't want to use sign up in background, then normal sign up is also available.
+                  user.signUpInBackground(new SignUpCallback() {
+                      public void done(ParseException e) {
+                          if (e == null) {
+                              // User registered
+
+                              // Hooray! Let them use the app now.
+                          } else {
+
+                              // Error Registering
+
+                              AlertDialog.Builder builder = new AlertDialog.Builder(getApplicationContext());
+                              builder.setTitle("Error")
+                                      .setMessage(e.getMessage().toString())
+                                      .setCancelable(false)
+                                      .setNegativeButton("Okay",new DialogInterface.OnClickListener() {
+                                          public void onClick(DialogInterface dialog, int id) {
+                                              dialog.cancel();
+                                          }
+                                      });
+                              AlertDialog alert = builder.create();
+                              alert.show();
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_register, menu);
-        return true;
-    }
+                          }
+                      }
+                  });
+              }
+              else
+              {
+
+                  // Alert message for missing value in textfield
+                  AlertDialog.Builder builder = new AlertDialog.Builder(getApplicationContext());
+                  builder.setTitle("Invalid Input")
+                          .setMessage("Please enter all values")
+                          .setCancelable(false)
+                          .setNegativeButton("Close",new DialogInterface.OnClickListener() {
+                              public void onClick(DialogInterface dialog, int id) {
+                                  dialog.cancel();
+                              }
+                          });
+                  AlertDialog alert = builder.create();
+                  alert.show();
+              }
+        }
+    });
+
+
+}
 
 }

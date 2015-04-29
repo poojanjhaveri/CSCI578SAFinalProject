11ff174aa27e22924a683e4417774191a2131f70
Trina Gregory <trinagre@usc.edu>
2015-04-29 01:00:56 -0700

    SensorActivity
    My device only gets Light, not Temperature (or the other 2)

--- TrojaNow_csci578/app/src/main/java/edu/usc/poojan/trojanow/SensorActivity.java
+++ TrojaNow_csci578/app/src/main/java/edu/usc/poojan/trojanow/SensorActivity.java
@@ -9,14 +9,24 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+
+import java.util.List;
 
 
 public class SensorActivity extends Activity implements SensorEventListener {
     private SensorManager mSensorManager;
-    private Sensor mPressure;
     private Sensor mTemperature;
     private Sensor mLight;
+    private Sensor mPressure;
     private Sensor mHumidity;
+    private String mInfo;
+    private float  fTemperature = 0;
+    private float  fLight = 0;
+    private float  fPressure = 0;
+    private float  fHumidity = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,10 +36,117 @@ public class SensorActivity extends Activity implements SensorEventListener {
         // Get an instance of the sensor service, and use that to get an instance of
         // a particular sensor.
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        mPressure = mSensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE);
         mTemperature = mSensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
         mLight = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        mPressure = mSensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE);
         mHumidity = mSensorManager.getDefaultSensor(Sensor.TYPE_RELATIVE_HUMIDITY);
+
+        final CheckBox tempCB = (CheckBox) findViewById(R.id.checkBoxSensor1);
+        if (mTemperature != null) {
+
+            tempCB.setEnabled(true);
+
+            tempCB.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    if (tempCB.isChecked()) {
+                        System.out.println("tempCB: isChecked");
+                        startSensor(mTemperature);
+                    } else {
+                        System.out.println("tempCB: isNotChecked");
+                        stopSensor(mTemperature);
+                    }
+
+                }
+
+            });
+        } else {
+            tempCB.setChecked(false);
+            tempCB.setEnabled(false);
+        }
+
+        final CheckBox lightCB = (CheckBox) findViewById(R.id.checkBoxSensor2);
+        if (mLight != null) {
+
+            lightCB.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (lightCB.isChecked()) {
+                        startSensor(mLight);
+                    } else {
+                        stopSensor(mLight);
+                    }
+                }
+
+            });
+        } else {
+            lightCB.setChecked(false);
+            lightCB.setEnabled(false);
+        }
+
+        final CheckBox pressureCB = (CheckBox) findViewById(R.id.checkBoxSensor3);
+        if (mPressure != null) {
+
+            pressureCB.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (pressureCB.isChecked()) {
+                        startSensor(mPressure);
+                    } else {
+                        stopSensor(mPressure);
+                    }
+                }
+
+            });
+        } else {
+            pressureCB.setChecked(false);
+            pressureCB.setEnabled(false);
+        }
+
+        final CheckBox humidityCB = (CheckBox) findViewById(R.id.checkBoxSensor4);
+        if (mHumidity != null) {
+
+            humidityCB.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (humidityCB.isChecked()) {
+                        startSensor(mHumidity);
+                    } else {
+                        stopSensor(mHumidity);
+                    }
+                }
+
+            });
+        } else {
+            humidityCB.setChecked(false);
+            humidityCB.setEnabled(false);
+        }
+
+        Button cancelButton = (Button) findViewById(R.id.sensorcancelbutton);
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+
+        Button sensorButton = (Button) findViewById(R.id.sensorinfobutton);
+        sensorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mInfo = "";
+                if (fTemperature != 0) {
+                    mInfo = mInfo.concat(" The temperature is " + fTemperature + " degrees Celsius.");
+                }
+                if (fLight != 0) {
+                    mInfo = mInfo.concat(" The light is " + fLight + " SI lux.");
+                }
+                System.out.println ("info = " + mInfo);
+                onBackPressed();
+            }
+        });
+
     }
 
 
@@ -59,21 +176,26 @@ public class SensorActivity extends Activity implements SensorEventListener {
     public void onSensorChanged(SensorEvent event) {
         //float millibars_of_pressure = event.values[0];
         int type = event.sensor.getType();
-        switch (type) {
-        case Sensor.TYPE_PRESSURE:
-            System.out.println("SensorActivity: Pressure");
-            break;
-        case Sensor.TYPE_AMBIENT_TEMPERATURE:
-            System.out.println("SensorActivity: Pressure");
-            break;
-        case Sensor.TYPE_LIGHT:
-            System.out.println("SensorActivity: Pressure");
-            break;
-        case Sensor.TYPE_RELATIVE_HUMIDITY:
-            System.out.println("SensorActivity: Pressure");
-            break;
-
+        if (type == Sensor.TYPE_AMBIENT_TEMPERATURE) {
+            System.out.println("SensorActivity: Temperature");
+            fTemperature = event.values[0];
         }
+
+        if (type == Sensor.TYPE_LIGHT) {
+            System.out.println("SensorActivity: Light");
+            fLight = event.values[0];
+        }
+
+        if (type == Sensor.TYPE_PRESSURE) {
+            System.out.println("SensorActivity: Pressure");
+            fPressure = event.values[0];
+        }
+
+        if (type == Sensor.TYPE_RELATIVE_HUMIDITY) {
+            System.out.println("SensorActivity: Humidity");
+            fHumidity = event.values[0];
+        }
+
     }
 
     @Override
@@ -85,11 +207,13 @@ public class SensorActivity extends Activity implements SensorEventListener {
     protected void onResume() {
         // Register a listener for the sensor.
         super.onResume();
-        mSensorManager.registerListener(this, mPressure, SensorManager.SENSOR_DELAY_NORMAL);
+        /*
         mSensorManager.registerListener(this, mTemperature, SensorManager.SENSOR_DELAY_NORMAL);
+
         mSensorManager.registerListener(this, mLight, SensorManager.SENSOR_DELAY_NORMAL);
         mSensorManager.registerListener(this, mHumidity, SensorManager.SENSOR_DELAY_NORMAL);
-
+        mSensorManager.registerListener(this, mPressure, SensorManager.SENSOR_DELAY_NORMAL);
+        */
 
     }
 
@@ -97,10 +221,18 @@ public class SensorActivity extends Activity implements SensorEventListener {
     protected void onPause() {
         // Be sure to unregister the sensor when the activity pauses.
         super.onPause();
-        mSensorManager.unregisterListener(this, mPressure);
         mSensorManager.unregisterListener(this, mTemperature);
         mSensorManager.unregisterListener(this, mLight);
+        mSensorManager.unregisterListener(this, mPressure);
         mSensorManager.unregisterListener(this, mHumidity);
 
+    }
+
+    protected void startSensor (Sensor mSensor) {
+        mSensorManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    protected void stopSensor (Sensor mSensor) {
+        mSensorManager.unregisterListener(this, mSensor);
     }
 }
